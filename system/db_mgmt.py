f78948a0ddd7424b16e6347b5c47cca00fc46370
Dylan Stein <djstein@ncsu.edu>
2016-11-12 16:56:44 -0500

    Skeleton for commands and quit methods

--- system/db_mgmt.py
+++ system/db_mgmt.py
@@ -1,2 +1,19 @@
+import os
+import sqlite3
+import sys
+
+def commands():
+    print(
+        """WolfVillas Database Management System
+    Version: 1.0.0
+    Commands:
+        quit: end the current session
+        help: prompt the commands"""
+    )
+
+def quit():
+    sys.exit()
+
 if __name__ == '__main__':
-    print "WolfVillas Database Managment System"
+    commands()
+    quit()
